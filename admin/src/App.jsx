243dd57f49036da362923648e15ebc177cdb4458
Yahia-mati <yahyamati8@gmail.com>
2024-08-12 15:40:26 +0100

    fix error

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url = "http://localhost:4000"; // Adjust this to your backend URL
+  const url = "http://:4000"; // Adjust this to your backend URL
   return (
     <div>
     <ToastContainer />
